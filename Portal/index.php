967fac0f75750b74bcc2df00fe8691059e2da53a
Swaroop-bhat <92312437+Swaroop-bhat@users.noreply.github.com>
2023-05-04 10:15:47 +0530

    First commit

---
+++ Portal/index.php
@@ -0,0 +1,136 @@
+<?php
+  include('./dbConnection.php');
+  
+  include('./mainInclude/header.php'); 
+?>  
+   <br>
+    <div class="container-fluid remove-vid-marg py-4">
+      <div class="vid-parent">
+        <img src="elearning-getty.jpg" alt="" width="100%" height="600px">
+      <div class="vid-content" >
+        <h1 class="my-content" style="color: black;">Portal to learn engineering in kannada</h1>
+        <small class="my-content"></small><br />
+        <?php    
+              if(!isset($_SESSION['is_login'])){
+                echo '<a class="btn btn-danger mt-3" href="#" data-toggle="modal" data-target="#stuRegModalCenter">Get Started</a>';
+              } else {
+                echo '<a class="btn btn-primary mt-3" href="student/studentProfile.php">My Profile</a>';
+              }
+          ?> 
+       
+      </div>
+    </div> 
+
+    
+    
+    <div class="container mt-5"> 
+      <h1 class="text-center">Popular Course</h1>
+      <div class="card-deck mt-4"> 
+        <?php
+        $sql = "SELECT * FROM course LIMIT 3";
+        $result = $conn->query($sql);
+        if($result->num_rows > 0){ 
+          while($row = $result->fetch_assoc()){
+            $course_id = $row['course_id'];
+            echo '
+            <a href="coursedetails.php?course_id='.$course_id.'" class="btn" style="text-align: left; padding:0px; margin:0px;">
+              <div class="card">
+                <img src="'.str_replace('..', '.', $row['course_img']).'" class="card-img-top" alt="Guitar" />
+                <div class="card-body">
+                  <h5 class="card-title">'.$row['course_name'].'</h5>
+                  <p class="card-text">'.$row['course_desc'].'</p>
+                </div>
+                <div class="card-footer">
+                  <p class="card-text d-inline">Price: <small><del>&#8377 '.$row['course_original_price'].'</del></small> <span class="font-weight-bolder">&#8377 '.$row['course_price'].'<span></p> <a class="btn btn-primary text-white font-weight-bolder float-right" href="coursedetails.php?course_id='.$course_id.'">Enroll</a>
+                </div>
+              </div>
+            </a>  ';
+          }
+        }
+        ?>   
+      </div>    
+      <div class="card-deck mt-4"> 
+        <?php
+          $sql = "SELECT * FROM course LIMIT 3,3";
+          $result = $conn->query($sql);
+          if($result->num_rows > 0){ 
+            while($row = $result->fetch_assoc()){
+              $course_id = $row['course_id'];
+              echo '
+                <a href="coursedetails.php?course_id='.$course_id.'"  class="btn" style="text-align: left; padding:0px;">
+                  <div class="card">
+                    <img src="'.str_replace('..', '.', $row['course_img']).'" class="card-img-top" alt="Guitar" />
+                    <div class="card-body">
+                      <h5 class="card-title">'.$row['course_name'].'</h5>
+                      <p class="card-text">'.$row['course_desc'].'</p>
+                    </div>
+                    <div class="card-footer">
+                      <p class="card-text d-inline"></p> <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+                    </div>
+                  </div>
+                </a>  ';
+            }
+          }
+        ?>
+      </div>   
+      <div class="text-center m-2">
+        <a class="btn btn-danger btn-sm" href="courses.php">View All Course</a> 
+      </div>
+    </div>  
+
+    <?php 
+    include('./contact.php'); 
+    ?>  
+
+      <div class="container-fluid mt-5" style="background-color: #4B7289" id="Feedback">
+        <h1 class="text-center testyheading p-4"> Student's Feedback </h1>
+        <div class="row">
+          <div class="col-md-12">
+            <div id="testimonial-slider" class="owl-carousel">
+            <?php 
+              $sql = "SELECT s.stu_name, s.stu_occ, s.stu_img, f.f_content FROM student AS s JOIN feedback AS f ON s.stu_id = f.stu_id";
+              $result = $conn->query($sql);
+              if($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()){
+                  $s_img = $row['stu_img'];
+                  $n_img = str_replace('../','',$s_img)
+            ?>
+              <div class="testimonial">
+                <p class="description">
+                <?php echo $row['f_content'];?>  
+                </p>
+                <div class="pic">
+                  <img src="<?php echo $n_img; ?>" alt=""/>
+                </div>
+                <div class="testimonial-prof">
+                  <h4><?php echo $row['stu_name']; ?></h4>
+                  <small><?php echo $row['stu_occ']; ?></small>
+                </div>
+              </div>
+              <?php }} ?>
+            </div>
+          </div>
+        </div>
+    </div>  
+
+    <div class="container-fluid bg-danger"> 
+        <div class="row text-white text-center p-1">
+          <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-facebook-f"></i> Facebook</a>
+          </div>
+          <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-twitter"></i> Twitter</a>
+          </div>
+          <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-whatsapp"></i> WhatsApp</a>
+          </div>
+          <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-instagram"></i> Instagram</a>
+          </div>
+        </div>
+    </div> 
+  <?php 
+
+    include('./mainInclude/footer.php'); 
+    
+  ?>  
